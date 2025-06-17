6e7f79f1c311d0f8b52022a40b078b971279cd41
Karen Payne <paynekaren@comcast.net>
2025-06-17 14:33:46 -0700

    Add development logging configuration in Program.cs
    This update introduces a conditional statement in `Program.cs` to check if the application is in a development environment. If true, it configures the logger to set minimum logging levels for "Microsoft" and "System" namespaces to warning. This change enhances log management during development.
    
    by Karen

--- TemplateExample/Program.cs
+++ TemplateExample/Program.cs
@@ -11,6 +11,7 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
         if (builder.Environment.IsDevelopment())
         {
             Log.Logger = new LoggerConfiguration()
